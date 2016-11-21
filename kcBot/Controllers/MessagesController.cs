137bbc759d99d27dd0bfe34050e0a2765548f1e1
Karim <kcis880@aucklanduni.ac.nz>
2016-11-22 01:33:33 +1300

    The chat bot can now return the current price of a stock and a chart

--- kcBot/Controllers/MessagesController.cs
+++ kcBot/Controllers/MessagesController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using kcBot;
+using System.Collections.Generic;
 
 namespace kcBot
 {
@@ -22,12 +24,42 @@ namespace kcBot
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters!");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                string companyName = activity.Text;
+                Activity weatherReply = activity.CreateReply($"{companyName} stock");
+                weatherReply.Recipient = activity.From;
+                weatherReply.Type = "message";
+                weatherReply.Attachments = new List<Attachment>();
+
+                List<CardImage> cardImages = new List<CardImage>();
+                cardImages.Add(new CardImage(url: $"http://ichart.finance.yahoo.com/b?s={companyName}"));
+
+                List<CardAction> cardButtons = new List<CardAction>();
+                CardAction plButton = new CardAction()
+                {
+                    Value = $"http://money.cnn.com/quote/quote.html?symb={companyName}",
+                    Type = "openUrl",
+                    Title = "More Info"
+                };
+                cardButtons.Add(plButton);
+                string stockInfo = await Sotck.getStockAsync(activity.Text);
+                HeroCard plCard = new HeroCard()
+                {
+                    Title = stockInfo,
+                    Images = cardImages,
+                    Buttons = cardButtons
+                };
+
+                Attachment plAttachment = plCard.ToAttachment();
+                weatherReply.Attachments.Add(plAttachment);
+                await connector.Conversations.SendToConversationAsync(weatherReply);
+
+                // return our reply to the user
+                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters!");
+                //string returnString = await Sotck.getStockAsync(activity.Text);
+                //Activity reply = activity.CreateReply(returnString);
+                //await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
             {
