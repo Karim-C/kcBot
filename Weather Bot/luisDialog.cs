154c82cc1e38d59ce81a106cc9a26adc072be924
Karim <kcis880@aucklanduni.ac.nz>
2016-11-25 00:38:39 +1300

    tranfering from person to another in the database works now

--- Weather Bot/luisDialog.cs
+++ Weather Bot/luisDialog.cs
@@ -5,6 +5,7 @@ using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
 using System.EnterpriseServices;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace kcBot
@@ -49,8 +50,8 @@ namespace kcBot
         public async Task login(IDialogContext context, LuisResult result)
         {
 
-            PromptDialog.Text(context, ResumeAfterUserName, "Please enter your name", "Try again",3);
-           
+            PromptDialog.Text(context, ResumeAfterUserName, "Please enter your name", "Try again", 3);
+
         }
 
         private async Task ResumeAfterUserName(IDialogContext context, IAwaitable<string> answer)
@@ -61,7 +62,7 @@ namespace kcBot
             var text = $"Name: {name}";
             await context.PostAsync(text);
             PromptDialog.Text(context, ResumeAfterPassword, "Please enter your password", "Try again", 3);
-           // context.Wait(MessageReceived);
+            // context.Wait(MessageReceived);
         }
 
         private async Task ResumeAfterPassword(IDialogContext context, IAwaitable<string> answer)
@@ -78,12 +79,13 @@ namespace kcBot
             {
                 context.UserData.SetValue("loggedIn", true);
                 endOutput = $"You have successfully loged in {userName}";
-            }else
+            }
+            else
             {
                 endOutput = $"You were not successfully at login in";
             }
 
-            
+
             await context.PostAsync(endOutput);
             context.Wait(MessageReceived);
         }
@@ -178,42 +180,50 @@ namespace kcBot
 
 
         [LuisIntent("transfer")]
-        public async Task RemoveTeam(IDialogContext context, LuisResult result)
+        public async Task transfer(IDialogContext context, LuisResult result)
         {
-
-            EntityRecommendation rec;
-            if (result.TryFindEntity("TeamName", out rec))
+            string userName = "";
+            if (context.UserData.TryGetValue("UserName", out userName))
             {
-                string teamName = rec.Entity;
-                if (true)
+                double transferAmount = 0;
+                string strAmount = result.Entities.FirstOrDefault(e => e.Type == "number").Entity;
+                string reciever = result.Entities.FirstOrDefault(e => e.Type == "reciever").Entity;
+
+                if (result.Entities.Count == 2 && double.TryParse(strAmount, out transferAmount))
                 {
-                    PromptDialog.Confirm(context, RemoveTeamAsync, $"Are you sure you want to delete {teamName}?");
+                    double currentBalance = await AzureManager.AzureManagerInstance.getBalance(userName);
+                    if (currentBalance > transferAmount)
+                    {
+                        // here the database records are up dated to reflect the transfer
+                        // reciever = result.Entities[1].Entity;
+                        await AzureManager.AzureManagerInstance.updateBalance(reciever, transferAmount);
+                        transferAmount = transferAmount * -1;
+                        await AzureManager.AzureManagerInstance.updateBalance(userName, transferAmount);
+                    }
+                    else
+                    {
+                        string endOutput = "You do not have sufficient funds";
+                        await context.PostAsync(endOutput);
+                        context.Wait(MessageReceived);
+                    }
                 }
                 else
                 {
-                    await context.PostAsync($"The team { teamName } was not found.");
+                    string endOutput = "Please try again (e.g. I want to transfer 100 dallors to Bob)";
+                    await context.PostAsync(endOutput);
                     context.Wait(MessageReceived);
                 }
+
+
             }
             else
             {
-                await context.PostAsync("You do not have enough funds to make this transfer.");
+                string endOutput = "Please login before trying to make a transfer";
+                await context.PostAsync(endOutput);
                 context.Wait(MessageReceived);
             }
+
         }
 
-        private async Task RemoveTeamAsync(IDialogContext context, IAwaitable<bool> result)
-        {
-            if (await result)
-            {
-
-                await context.PostAsync($"has been removed.");
-            }
-            else
-            {
-                await context.PostAsync($"OK, we wont remove them.");
-            }
-            context.Wait(MessageReceived);
-        }
-    }
+    }   
 }
